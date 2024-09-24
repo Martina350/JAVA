ffa0b0d326fc5edab6a83f83e5837c8c2e0c1042
Martina350 <martinacamilaleon@gmail.com>
2024-09-24 18:07:39 -0500

    SE ENCAPSULA LA CLASE PRODUCTO Y SE MODIFICA EL TESTPRODUCTO TODO FUNCIONA

--- eclipse-workspace/Clases/src/com/krakedev/TestProducto.java
+++ eclipse-workspace/Clases/src/com/krakedev/TestProducto.java
@@ -7,38 +7,38 @@ public class TestProducto {
 		Producto productoB=new Producto();
 		Producto productoC=new Producto();
 		
-		productoA.nombre="Aspirina";
-		productoA.descripcion="Pastillas";
-		productoA.precio=14.99;
-		productoA.stockAnual=189;
+		productoA.setNombre("Aspirina");
+		productoA.setDescripcion("Pastillas");
+		productoA.setPrecio(14.99);
+		productoA.setStockAnual(189);
 		
 		System.out.println("------");
-		System.out.println("nombre:"+productoA.nombre);
-		System.out.println("descripcion:"+productoA.descripcion);
-		System.out.println("precio:"+productoA.precio);
-		System.out.println("stockAnual:"+productoA.stockAnual);
+		System.out.println("nombre:"+productoA.getNombre());
+		System.out.println("descripcion:"+productoA.getDescripcion());
+		System.out.println("precio:"+productoA.getPrecio());
+		System.out.println("stockAnual:"+productoA.getStockAnual());
 		
-		productoB.nombre="Buprex";
-		productoB.descripcion="Jarabe";
-		productoB.precio=12.55;
-		productoB.stockAnual=145;
-		
-		System.out.println("------");
-		System.out.println("nombre:"+productoB.nombre);
-		System.out.println("descripcion:"+productoB.descripcion);
-		System.out.println("precio:"+productoB.precio);
-		System.out.println("stockAnual:"+productoB.stockAnual);
-		
-		productoC.nombre="Eucamiel";
-		productoC.descripcion="Jarabe";
-		productoC.precio=8.55;
-		productoC.stockAnual=125;
+		productoB.setNombre("Buprex");
+		productoB.setDescripcion("Jarabe");
+		productoB.setPrecio(12.55);
+		productoB.setStockAnual(145);
 		
 		System.out.println("------");
-		System.out.println("nombre:"+productoC.nombre);
-		System.out.println("descripcion:"+productoC.descripcion);
-		System.out.println("precio:"+productoC.precio);
-		System.out.println("stockAnual:"+productoC.stockAnual);
+		System.out.println("nombre:"+productoB.getNombre());
+		System.out.println("descripcion:"+productoB.getDescripcion());
+		System.out.println("precio:"+productoB.getPrecio());
+		System.out.println("stockAnual:"+productoB.getStockAnual());
+		
+		productoC.setNombre("Eucamiel");
+		productoC.setDescripcion("Jarabe");
+		productoC.setPrecio(8.55);
+		productoC.setStockAnual(125);
+		
+		System.out.println("------");
+		System.out.println("nombre:"+productoC.getNombre());
+		System.out.println("descripcion:"+productoC.getDescripcion());
+		System.out.println("precio:"+productoC.getPrecio());
+		System.out.println("stockAnual:"+productoC.getStockAnual());
 	}
 
 }
