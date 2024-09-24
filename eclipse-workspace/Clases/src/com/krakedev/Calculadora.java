91565949e887cf3f9df54efce7322fbd156b7988
Martina350 <martinacamilaleon@gmail.com>
2024-09-23 21:40:04 -0500

    SE AGREGAN LAS FUNCIONES SOLICITADAS, FUNCIONAN CORRECTAMENTE

--- eclipse-workspace/Clases/src/com/krakedev/Calculadora.java
+++ eclipse-workspace/Clases/src/com/krakedev/Calculadora.java
@@ -11,4 +11,25 @@ public class Calculadora {
 		resultado=valor1-valor2;
 		return resultado;
 	}
+	public double multiplicar(double valor1, double valor2) {
+		double resultado;
+		resultado=valor1*valor2;
+		return resultado;
+	}
+	public double dividir(double dividiendo, double divisor) {
+		double resultado;
+		resultado=dividiendo/divisor;
+		return resultado;
+	}
+	public double promediar(double valor1,double valor2, double valor3) {
+		double  resultadoTotal;
+		double promedioTotal;
+		resultadoTotal=valor1+valor2+valor3;
+		promedioTotal=resultadoTotal/3;
+		return promedioTotal;
+	}
+	public void mostrarResultado() {
+			System.out.println("Ahorita no joven, sali al almuerzo. Regreso en 15 minutos");
+	}
 }
+
--- eclipse-workspace/Clases/src/com/krakedev/TestCalculadora.java
+++ eclipse-workspace/Clases/src/com/krakedev/TestCalculadora.java
@@ -6,11 +6,21 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		int resultadoResta;
+		double resultadoMultiplicacion;
+		double resultadoDivision;
+		double resultadoTotalPromedio;
 		calcu=new Calculadora();
 		resultadoSuma=calcu.sumar(5, 3);
 		resultadoResta=calcu.restar(10, 3);
+		resultadoMultiplicacion=calcu.multiplicar(10, 5);
+		resultadoDivision=calcu.dividir(10, 2);
+		resultadoTotalPromedio=calcu.promediar(10, 8, 9);
+		calcu.mostrarResultado();
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
+		System.out.println("RESULTADO DIVISION: "+resultadoDivision);
+		System.out.println("RESULTADO DIVISION: "+resultadoTotalPromedio);
 	}
 
 }
