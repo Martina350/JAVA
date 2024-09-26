e20ea02869017d0e93a3d6031f83095cc0d12938
Martina350 <martinacamilaleon@gmail.com>
2024-09-26 17:20:58 -0500

    CUENTAS Y TESTCUENTAS PARTE A, B, C, D Y E EN PROCESO FUNCIONA

--- eclipse-workspace/Taller/src/clearmind/cuentas/Cuentas.java
+++ eclipse-workspace/Taller/src/clearmind/cuentas/Cuentas.java
@@ -1,5 +1,62 @@
 package clearmind.cuentas;
 
 public class Cuentas {
+	private String id;
+	private String tipo;
+	private double saldo;
 	
+	
+	public Cuentas (String id, String tipo) {
+		this.id=id;
+		this.tipo="A";
+	}
+	
+	public Cuentas(String id, String tipo, double saldo) {
+	    this.id=id;
+	    this.tipo=tipo;
+	    this.saldo=saldo;
+	}
+	
+	public void imprimir () {
+		System.out.println("**********************");
+		System.out.println("        CUENTA        ");
+		System.out.println("**********************");
+	    System.out.println();
+	    System.out.println("Número de Cuenta: "+id);
+	    System.out.println("Tipo: "+tipo);
+	    System.out.println("Saldo: USD "+saldo);
+	    System.out.println();
+	    System.out.println("********************");
+	}
+	
+	public void imprimirConMiEstilo() {
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║        INFORMACIÓN DE        ║");
+        System.out.println("║          LA CUENTA           ║");
+        System.out.println("╚══════════════════════════════╝");
+        System.out.println();
+        System.out.println("✦ Número de Cuenta: "+id);
+        System.out.println("✦ Tipo: "+tipo);
+        System.out.println("✦ Saldo Actual:"+saldo);
+        System.out.println();
+        System.out.println("╚══════════════════════════════╝");
+    }
+	
+	public String getId() {
+		return id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
 }
--- eclipse-workspace/Taller/src/clearmind/cuentas/test/TestCuentas.java
+++ eclipse-workspace/Taller/src/clearmind/cuentas/test/TestCuentas.java
@@ -1,10 +1,20 @@
 package clearmind.cuentas.test;
 
+import clearmind.cuentas.Cuentas;
+
 public class TestCuentas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cuentas cuenta1=new Cuentas("03476","");
+		cuenta1.setSaldo(675);
+		Cuentas cuenta2=new Cuentas("03476","D",98);
+		Cuentas cuenta3=new Cuentas("03476","");
+		cuenta3.setTipo("C");
+		
+		System.out.println("-----Valores Iniciales-----");
+		cuenta1.imprimir();
+		cuenta2.imprimir();
+		cuenta3.imprimir();
+		System.out.println("-----Valores Modificados-----");
 	}
-
 }
