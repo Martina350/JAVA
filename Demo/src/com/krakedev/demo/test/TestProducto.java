48bbecd79fa8abcd6daa791f9a529e8c3811ceeb
Martina350 <martinacamilaleon@gmail.com>
2024-09-26 20:26:26 -0500

    SE CREA EL PACKAGE DEFINITIVO DE PRODUCTOS Y TESTPRODUCTOS TODO FUNCIONA

--- Demo/src/com/krakedev/demo/test/TestProducto.java
+++ Demo/src/com/krakedev/demo/test/TestProducto.java
@@ -2,18 +2,18 @@ package com.krakedev.demo.test;
 
 import com.krakedev.demo.Producto;
 
-public class TestProductos {
+public class TestProducto {
 
 	public static void main(String[] args) {
-		Producto producto1=new Producto(101,"Manzana");
+		Producto producto1= new Producto(523,"Manzana");
 		
 		producto1.setDescripcion("Organica");
-		producto1.setPeso(180);
+		producto1.setPeso(0.8);
 		
 		System.out.println("CODIGO DEL PRODUCTO: "+producto1.getCodigo());
 		System.out.println("NOMBRE DEL PRODUCTO: "+producto1.getNombre());
-		System.out.println("DESCRIPCION: "+producto1.getCodigo());
-		System.out.println("PESO:"+producto1.getCodigo());
+		System.out.println("DESCRIPCION: "+producto1.getDescripcion());
+		System.out.println("PESO: "+producto1.getPeso());
 	}
 
 }
