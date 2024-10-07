64b1bf0ec08727afe64e53c6a3fd28fab34b77fe
Martina350 <martinacamilaleon@gmail.com>
2024-10-06 20:59:33 -0500

    SE CREA LA CARPETA EVALUACION1, SE AGREGA LA CLASE PRODUCTO Y TEST PRODUCTO, FUNCIONAN CORRECTAMENTE

---
+++ Evaluacion1MartinaLeon/src/com/clearminds/componentes/Producto.java
@@ -0,0 +1,42 @@
+package com.clearminds.componentes;
+
+public class Producto {
+	private String nombre;
+	private double precio;
+	private String codigo;
+	
+	public void incrementarPrecio(int porcentajeIncremento) {
+		this.precio = this.precio + (this.precio * porcentajeIncremento / 100);
+	}
+
+	public void disminuirPrecio(double porcentajeDisminucion) {
+		this.precio = this.precio - porcentajeDisminucion;
+	}
+
+	
+	public Producto(String nombre, double precio, String codigo) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+}
---
+++ Evaluacion1MartinaLeon/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Evaluacion1MartinaLeon {
+}
