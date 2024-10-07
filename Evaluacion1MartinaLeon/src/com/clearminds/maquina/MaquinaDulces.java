14a93f091879e2dfcded6bacc7d83ade8f7c69e4
Martina350 <martinacamilaleon@gmail.com>
2024-10-06 22:59:26 -0500

    EJERCICIO 7,8,9,10 Y 11 REALIZADOS Y FUNCIONAN CORRECTAMENTE

--- Evaluacion1MartinaLeon/src/com/clearminds/maquina/MaquinaDulces.java
+++ Evaluacion1MartinaLeon/src/com/clearminds/maquina/MaquinaDulces.java
@@ -57,6 +57,70 @@ public class MaquinaDulces {
 	            System.out.println(" Sin Producto asignado");
 	        }
 	    }
+	    System.out.println("Saldo: " + saldo);
 	}
+	
+	public Producto buscarProductoEnCelda(String codigoCelda) {
+	    Celda celda = buscarCelda(codigoCelda);
+	    if (celda != null) {
+	        return celda.getProducto();
+	    }
+	    return null;
+	}
+	
+	public double consultarPrecio(String codigoCelda) {
+	    Celda celda = buscarCelda(codigoCelda); 
+	    if (celda != null && celda.getProducto() != null) { 
+	        return celda.getProducto().getPrecio(); 
+	    } else {
+	        System.out.println("Celda o producto no encontrado.");
+	        return 0.0; 
+	    }
+	}
+	
+	public Celda buscarCeldaProducto(String codigoProducto) {
+	    Celda celdaEncontrada = null; 
+	    Celda elementoCelda = null;   
+	    for (int i = 0; i < celdas.size(); i++) {
+	        elementoCelda = celdas.get(i); 
+	        Producto producto = elementoCelda.getProducto();  
+	        if (producto != null && codigoProducto.equals(producto.getCodigo())) { 
+	            celdaEncontrada = elementoCelda; 
+	        }
+	    }
+	    return celdaEncontrada;
+	}
+	
+	public void incrementarProductos(String codigoProducto, int cantidad) {
+	    Celda celdaEncontrada = buscarCeldaProducto(codigoProducto);
+
+	    if (celdaEncontrada != null) {
+	        int stockActual = celdaEncontrada.getStock();
+	        celdaEncontrada.setStock(stockActual + cantidad); 
+	        Producto producto = celdaEncontrada.getProducto(); 
+	        if (producto != null) {
+	            System.out.println("Celda:" + celdaEncontrada.getCodigo() + 
+	                               " Stock:" + celdaEncontrada.getStock() +
+	                               " Producto:" + producto.getNombre() + 
+	                               " Precio:" + producto.getPrecio());
+	        } else {
+	            System.out.println("Celda:" + celdaEncontrada.getCodigo() + 
+	                               " Stock:" + celdaEncontrada.getStock() + 
+	                               " Sin Producto asignado");
+	        }
+	    }
+	}
+	
+	 public void vender(String codigoCelda) {
+	        Celda celda = buscarCelda(codigoCelda);
+	        if (celda != null && celda.getStock() > 0) {
+	            Producto producto = celda.getProducto();
+	            if (producto != null) {
+	                celda.setStock(celda.getStock() - 1);
+	                saldo += producto.getPrecio();
+	            }
+	        }
+	 }
+	 
 	
 }
